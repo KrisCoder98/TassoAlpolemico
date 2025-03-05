78ba87cead4928d85138cf6410f146921ec77e14
Kris <kristian.scarpellino@gmail.com>
2025-03-05 23:51:29 +0100

    Link di recupero immagine

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -3,7 +3,7 @@ function TeamCard({alias, children}){
     return (
         <div className="rounded-3xl h-[300px] bg-white/70 hover:shadow-green-900 grid grid-cols-2">
             
-            <img src={`../public/img/${alias}Pic.jpg`} alt="" className="rounded-3xl absolute h-[300px] opacity-50 hover:opacity-100"/>
+            <img src={`/img/${alias}Pic.jpg`} alt="" className="rounded-3xl absolute h-[300px] opacity-50 hover:opacity-100"/>
             
             <p className="text-5xl text-blue-600 font-bold text-center">{alias}</p>
 
