837ce5d296b00520103027d45106f757034d78a3
Kris <kristian.scarpellino@gmail.com>
2025-03-05 23:28:33 +0100

    json mngmnt

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -1,11 +1,9 @@
-function TeamCard({alias, name, surname, children}){
+function TeamCard({alias, children}){
 
-    const completeName = `${name} ${surname}`; 
     return (
         <div className="rounded-3xl h-[300px] bg-white/70 hover:shadow-green-900 grid grid-cols-2">
             <img src={`src/img/${alias}Pic.jpg`} alt="" className="rounded-3xl absolute h-[300px] opacity-50 hover:opacity-100"/>
             <p className="text-5xl text-blue-600 font-bold text-center">{alias}</p>
-            {/* <p className="text-2xl text-gray-600">{completeName}</p> */}
 
             {(children) && <div className="text-white hover:bg-gray-700 p-2 rounded-r-3xl">{children}</div>}
         </div>
