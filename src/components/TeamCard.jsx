918ec8ead41cc3976ca1a091e8a90763488ccee5
Kris <kristian.scarpellino@gmail.com>
2025-03-05 23:46:03 +0100

    File Mngmnt

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -2,7 +2,9 @@ function TeamCard({alias, children}){
 
     return (
         <div className="rounded-3xl h-[300px] bg-white/70 hover:shadow-green-900 grid grid-cols-2">
-            <img src={`src/assets/img/${alias}Pic.jpg`} alt="" className="rounded-3xl absolute h-[300px] opacity-50 hover:opacity-100"/>
+            
+            <img src={`../public/img/${alias}Pic.jpg`} alt="" className="rounded-3xl absolute h-[300px] opacity-50 hover:opacity-100"/>
+            
             <p className="text-5xl text-blue-600 font-bold text-center">{alias}</p>
 
             {(children) && <div className="text-white hover:bg-gray-700 p-2 rounded-r-3xl">{children}</div>}
